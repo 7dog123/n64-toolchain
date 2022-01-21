a38f82dac521a89770b741a45e231a5b848217ab
7dog123 <futuretrunks745@gmail.com>
2022-01-21 16:21:16 -0600

    Update build-toolchain.sh

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -25,12 +25,11 @@ set -e
   mkdir -p "$INSTALL_PATH" # But make sure the install path exists!
 
 TARGET="mips64-elf"
+TARG_XTRA_OPTS=""
 
 if [ ! -e /usr/bin/x86_64-w64-mingw32-gcc ]; then
   export CC=x86_64-w64-mingw32-gcc
   TARG_XTRA_OPTS="--host=x86_64-w64-mingw32"
-else
-  TARG_XTRA_OPTS=""
 fi
 
 BINUTILS_V=2.36.1 # linux works fine with 2.37 (but is it worth the effort?)
