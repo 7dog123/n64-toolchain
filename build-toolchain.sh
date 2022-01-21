7aff4f39cbe0387c1441a47b24044f3abd00787d
7dog123 <futuretrunks745@gmail.com>
2022-01-21 20:12:19 +0000

    try ubuntu cross compile

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -6,29 +6,32 @@
 
 # Before calling this script, make sure you have all required
 # dependency packages installed in your system.  On a Debian-based systems
-#   this is achieved by typing the following commands:
+# this is achieved by typing the following commands:
 #
 # sudo apt-get update && sudo apt-get upgrade
 # sudo apt-get install -yq wget bzip2 gcc g++ make file libmpfr-dev libmpc-dev zlib1g-dev texinfo git gcc-multilib
 
+# Bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
 # Exit script on error
 set -e
 
-TARGET="mips64-elf"
-#OSVER=$(uname)
+# Ensure you set 'N64_INST' before calling the script to change the default installation directory path
+  # by default it will presume 'usr/local/n64_toolchain'
+  INSTALL_PATH="${N64_INST:-/usr/local}"
+  # rm -rf "$INSTALL_PATH" # We should probably do a clean install?!
+  mkdir -p "$INSTALL_PATH" # But make sure the install path exists!
 
-INSTALL_PATH="${N64_INST:-/usr/local/n64_toolchain}"
-# rm -rf "$INSTALL_PATH" # We should probably do a clean install?!
-
-if [[ "$OSTYPE" == "msys"* ]]; then
-       mkdir -p "$INSTALL_PATH" # But make sure the install path exists!
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-       sudo mkdir -p "$INSTALL_PATH" # But make sure the install path exists!
+if [ ! -x x86_64-w64-mingw32-gcc ] then;
+  export CC=x86_64-w64-mingw32-gcc
+  TARG_XTRA_OPTS="--host=x86_64-w64-mingw32"
 else
-       echo "Unknown OS"
+  TARG_XTRA_OPTS=""
 fi
 
-BINUTILS_V=2.36.1
+BINUTILS_V=2.36.1 # linux works fine with 2.37 (but is it worth the effort?)
 GCC_V=11.2.0
 NEWLIB_V=4.1.0
 
@@ -55,43 +58,43 @@ download () {
 }
 
 echo "Stage: Download and extract dependencies"
-
-echo "Download and extract binutils-$BINUTILS_V"
 test -f "binutils-$BINUTILS_V.tar.gz" || download "https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_V.tar.gz"
 test -d "binutils-$BINUTILS_V"        || tar -xzf "binutils-$BINUTILS_V.tar.gz"
 
-echo "Download and extract gcc-$GCC_V"
 test -f "gcc-$GCC_V.tar.gz"           || download "https://ftp.gnu.org/gnu/gcc/gcc-$GCC_V/gcc-$GCC_V.tar.gz"
 test -d "gcc-$GCC_V"                  || tar -xzf "gcc-$GCC_V.tar.gz" --checkpoint=.100 # TODO: there must be a better way of showing progress (given such a large file)!
 
-echo "Download and extract newlib-$NEWLIB_V"
 test -f "newlib-$NEWLIB_V.tar.gz"     || download "https://sourceware.org/pub/newlib/newlib-$NEWLIB_V.tar.gz"
 test -d "newlib-$NEWLIB_V"            || tar -xzf "newlib-$NEWLIB_V.tar.gz"
 
-if [ "$OSTYPE" == "msys*"* ]; then
-GMP_V=6.2.0
-MPC_V=1.2.1
-MPFR_V=4.1.0
-MAKE_V=4.2.1
 # Optional dependency handling
 # Copies the FP libs into GCC sources so they are compiled as part of it
-echo "Download and extract gmp-$GMP_V"
-  test -f "gmp-$GMP_V.tar.bz2"         || download "https://ftp.gnu.org/gnu/gmp/gmp-$GMP_V.tar.bz2"
-  mkdir gcc-$GCC_V/gmp
-  test -d "gmp-$GMP_V"                || tar -xf "gmp-$GMP_V.tar.bz2" -C gcc-$GCC_V/gmp # note no .gz download file currently available
+if [ ! -x x86_64-w64-mingw32-gcc ]; then
+  GMP_V=6.2.0
+  MPC_V=1.2.1
+  MPFR_V=4.1.0
+  MAKE_V=4.2.1
+  test -f "gmp-$GMP_V.tar.xz"         || download "https://ftp.gnu.org/gnu/gmp/gmp-$GMP_V.tar.xz"
+  test -d "gmp-$GMP_V"                || tar -xf "gmp-$GMP_V.tar.xz" # note no .gz download file currently available
+  cd "gcc-$GCC_V"
+  ln -sf ../"gmp-$GMP_V" "gmp"
+  cd ..
 
-echo "Download and extract mpc-$MPC_V"
   test -f "mpc-$MPC_V.tar.gz"         || download "https://ftp.gnu.org/gnu/mpc/mpc-$MPC_V.tar.gz"
-  mkdir gcc-$GCC_V/mpc
-  test -d "mpc-$MPC_V"                || tar -xzf "mpc-$MPC_V.tar.gz" -C gcc-$GCC_V/mpc
+  test -d "mpc-$MPC_V"                || tar -xzf "mpc-$MPC_V.tar.gz"
+  cd "gcc-$GCC_V"
+  ln -sf ../"mpc-$MPC_V" "mpc"
+  cd ..
 
-echo "Download and extract mpfr-$MPFR_V"
   test -f "mpfr-$MPFR_V.tar.gz"       || download "https://ftp.gnu.org/gnu/mpfr/mpfr-$MPFR_V.tar.gz"
-  mkdir gcc-$GCC_V/mpfr
-  test -d "mpfr-$MPFR_V"              || tar -xzf "mpfr-$MPFR_V.tar.gz" -C gcc-$GCC_V/mpfr
-# Certain platforms might require Makefile cross compiling
+  test -d "mpfr-$MPFR_V"              || tar -xzf "mpfr-$MPFR_V.tar.gz"
+  cd "gcc-$GCC_V"
+  ln -sf ../"mpfr-$MPFR_V" "mpfr"
+  cd ..
+fi
 
-echo "Download and extract make-$MAKE_V"
+# Certain platforms might require Makefile cross compiling
+if [ ! -x x86_64-w64-mingw32-gcc ]; then
   test -f "make-$MAKE_V.tar.gz"       || download "https://ftp.gnu.org/gnu/make/make-$MAKE_V.tar.gz"
   test -d "make-$MAKE_V"              || tar -xzf "make-$MAKE_V.tar.gz"
 fi
@@ -99,35 +102,23 @@ fi
 echo "Stage: Compile toolchain"
 
 echo "Compiling binutils-$BINUTILS_V"
-
+# TODO why do we bother if we already have a good (compatible) binutils installed?! 
+# e.g. we could use apt-install binutils-mips-linux-gnu if the host is debian?!
+# This would seriously decrease build time.
 cd "binutils-$BINUTILS_V"
-if [ "$OSTYPE" == "msys"* ]; then
-     # Hack - see native package for details
-     sed -i "/ac_cpp=/s/\$CPPFLAGS/\$CPPFLAGS -O2/" libiberty/configure
-fi
-
 ./configure \
   --prefix="$INSTALL_PATH" \
   --target="$TARGET" \
   --with-cpu=mips64vr4300 \
-  --disable-werror
+  --disable-werror \
+  $TARG_XTRA_OPTS
+
 make -j "$JOBS"
-
-if [[ "$OSTYPE" == "msys"* ]]; then
-	make install-strip
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sudo make install-strip || su -c "make install-strip"
-else
-        echo "Do Nothing"
-fi
-
+make install-strip || sudo make install-strip || su -c "make install-strip"
 make distclean # Cleanup to ensure we can build it again
 echo "Finished Compiling binutils-$BINUTILS_V"
 
 echo "Compiling native build of GCC-$GCC_V for MIPS N64 - (pass 1) outside of the source tree"
-# TODO why do we bother if we already have a good (compatible) compiler installed?! 
-# e.g. we could use ` whereis` ?! it does not need to be up-to-date as we have a second pass?!
-# This would seriously decrease build time.
 cd ..
 rm -rf gcc_compile
 mkdir gcc_compile
@@ -148,19 +139,12 @@ cd gcc_compile
   --disable-win32-registry \
   --disable-nls \
   --disable-werror \
-  --with-system-zlib
+  --with-system-zlib \
+  $TARG_XTRA_OPTS
 make all-gcc -j "$JOBS"
 make all-target-libgcc -j "$JOBS"
-if [[ "$OSTYPE" == "msys"* ]]; then
-	make install-strip-gcc
-	make install-target-libgcc
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sudo make install-strip-gcc || su -c "make install-strip-gcc"
-	sudo make install-target-libgcc || su -c "make install-target-libgcc"
-else
-        echo "Do Nothing"
-fi
-
+make install-strip-gcc || sudo make install-strip-gcc || su -c "make install-strip-gcc"
+make install-target-libgcc || sudo make install-target-libgcc || su -c "make install-target-libgcc"
 echo "Finished Compiling GCC-$GCC_V for MIPS N64 - (pass 1) outside of the source tree"
 
 echo "Compiling newlib-$NEWLIB_V"
@@ -174,20 +158,40 @@ CFLAGS_FOR_TARGET="-DHAVE_ASSERT_FUNC -O2" ./configure \
   --with-cpu=mips64vr4300 \
   --disable-threads \
   --disable-libssp \
-  --disable-werror
+  --disable-werror \
+  $TARG_XTRA_OPTS
 make -j "$JOBS"
-
-if [[ "$OSTYPE" == "msys"* ]]; then
-	make install
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sudo env PATH="$PATH" make install || su -c "env PATH=\"$PATH\" make install"
-else
-      echo "Do Nothing"
-fi
-
+make install || sudo env PATH="$PATH" make install || su -c "env PATH=\"$PATH\" make install"
 echo "Finished Compiling newlib-$NEWLIB_V"
 
-if [ "$OSTYPE" == "msys"* ]; then
+echo "Compiling gcc-$GCC_V for MIPS N64 for host - (pass 2) outside of the source tree"
+cd ..
+rm -rf gcc_compile
+mkdir gcc_compile
+cd gcc_compile
+CFLAGS_FOR_TARGET="-O2" CXXFLAGS_FOR_TARGET=" -O2" ../"gcc-$GCC_V"/configure \
+  --prefix="$INSTALL_PATH" \
+  --target="$TARGET" \
+  --with-arch=vr4300 \
+  --with-tune=vr4300 \
+  --enable-languages=c,c++ \
+  --without-headers \
+  --with-newlib \
+  --disable-libssp \
+  --enable-multilib \
+  --disable-shared \
+  --with-gcc \
+  --disable-threads \
+  --disable-win32-registry \
+  --disable-nls \
+  --disable-werror \
+  --with-system-zlib \
+  $TARG_XTRA_OPTS
+make -j "$JOBS"
+make install-strip || sudo make install-strip || su -c "make install-strip"
+echo "Finished Compiling gcc-$GCC_V for MIPS N64 - (pass 2) outside of the source tree"
+
+if [ ! -x x86_64-w64-mingw32-gcc ]; then
 echo "Compiling make-$MAKE_V" # As make is otherwise not available on Windows
 cd ../"make-$MAKE_V"
   ./configure \
@@ -195,9 +199,15 @@ cd ../"make-$MAKE_V"
     --disable-largefile \
     --disable-nls \
     --disable-rpath \
+    $TARG_XTRA_OPTS
 make -j "$JOBS"
-make install-strip
+make install-strip || sudo make install-strip || su -c "make install-strip"
 make clean
-
 echo "Finished Compiling make-$MAKE_V"
+fi
+
+if [ ! -x x86_64-w64-mingw32-gcc ]; then
+  echo "Cross compile successful"
+else
+  echo "Native compile successful"
 fi
